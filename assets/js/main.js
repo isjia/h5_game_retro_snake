21312b6c5a4cc73111fea210ef16ce735023b049
Jia Qian <isjia@163.com>
2016-05-17 14:13:18 +0800

    finish initialise drawing the snake and apple

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,8 @@ game = new Phaser.Game(600, 450, Phaser.AUTO, '');
 // First parameter is how our state will be called.
 // Second parameter is an object containing the needed methods for state functionality
 game.state.add('Menu', Menu);
+
+// Adding the Game state.
+game.state.add('Game', Game);
+
 game.state.start('Menu');
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -9,6 +9,15 @@ var Menu = {
   create: function(){
     // Add a sprite to your game, here the sprite will be the game's logo
     // Parameters are: X, Y, image name (see above)
-    this.add.sprite(0, 0, 'menu');
+    // this.add.sprite(0, 0, 'menu');
+
+    // Add menu screen.
+    // It wil act as a button to start the game.
+    this.add.button(0,0,'menu', this.startGame, this);
+  },
+
+  startGame: function(){
+    // Change the state to the actual game.
+    this.state.start('Game');
   }
 }
